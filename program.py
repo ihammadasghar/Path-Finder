f4871dcfc30ee96acfcf0220e6756fddf963c996
Hammad Asghar <ihammadasghar@gmail.com>
2022-04-25 19:05:35 +0100

    Add settings.py

--- program.py
+++ program.py
@@ -1,9 +1,5 @@
 from gui import main
+from settings import *
 
-WIDTH = 700
-HEIGHT = 700
-DIMS = (50, 50)
-DIAGONAL_COST = 2.0
-ADJACENT_COST = 1.0
 
 main(WIDTH, HEIGHT, DIMS, DIAGONAL_COST, ADJACENT_COST)
